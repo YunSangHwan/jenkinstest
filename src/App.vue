57ae35dbc350435e99a89a50cd9818c9bf8b6ad5
조성현 <josunghyun0712@gmail.com>
2020-10-17 23:10:17 +0900

    aaaa

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app id="inspire">
     <!-- <router-view name="header"></router-view> -->
-    젠킨스 테스트44
+    젠킨스 테스트55
     <router-view name="content"></router-view>
   </v-app>
 </template>
